34f41d28f030f7a1a0ae41dbf3f317a0900b1742
radhouene101 <radhouene.berrzig@esprit.tn>
2024-10-13 13:09:04 +0200

    test jenkins

--- src/main/java/radhouene/develop/vcenter/vmwarevcenterautomation/globalVars/GlobalVars.java
+++ src/main/java/radhouene/develop/vcenter/vmwarevcenterautomation/globalVars/GlobalVars.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
+//test-pipeline
 public class GlobalVars {
     public static HttpHeaders cookies;
     public static String sessionId;
